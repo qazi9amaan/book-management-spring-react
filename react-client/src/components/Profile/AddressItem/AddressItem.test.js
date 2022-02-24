d4dac3f52c9769290762f0b681bb97656e070d2b
qazi amaan <qazi-amaan@qazi-amaan.local>
2022-02-25 00:33:17 +0530

    address done

---
+++ react-client/src/components/Profile/AddressItem/AddressItem.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { mount, shallow } from "enzyme";
+import {
+	checkProp,
+	findByTestAttr,
+} from "../../../tests/testUtils";
+import AddressItem from "./AddressItem";
+
+const defaultProps = {
+	address: {
+		aid: 39,
+		customerName: "Qazi Amaan",
+		phoneNumber: "7889776055",
+		pincode: "121211",
+		mstate: "",
+		address: "sdasdas",
+	},
+};
+
+const setup = (props = {}) => {
+	const setUpProps = { ...defaultProps, ...props };
+	return shallow(<AddressItem {...setUpProps} />);
+};
+
+describe("Address item component test casses", () => {
+	it("renders without any error", async () => {
+		const wrapper = setup();
+		const component = await findByTestAttr(
+			wrapper,
+			"component-address-item"
+		);
+		expect(component.length).toBe(1);
+	});
+
+	it("doesnot throw warning with expected props", async () => {
+		checkProp(AddressItem, defaultProps);
+	});
+});
